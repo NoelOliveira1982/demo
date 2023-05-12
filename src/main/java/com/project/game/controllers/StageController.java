2394c762391a60f932e2d16369686bdedce36a59
Jorge Alberto Oliveira <sieghart1982@gmail.com>
2023-05-11 22:12:34 -0300

    Update: Player Movement Complete

--- src/main/java/com/project/game/controllers/StageController.java
+++ src/main/java/com/project/game/controllers/StageController.java
@@ -8,6 +8,7 @@ import com.project.game.models.Character;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 
 public class StageController implements Initializable {
 
@@ -21,5 +22,13 @@ public class StageController implements Initializable {
     public void initialize(URL arg0, ResourceBundle arg1) {
         player1 = new  Character("Teste", player1Shape);
         player1.playIdleAnimation();
+    }
+
+    public void controlPlayerPressed(KeyEvent event) {
+        KeyboardController.keyboardHandlerPressed(event, player1);
+    }
+
+    public void controlPlayerReleased(KeyEvent event){
+        KeyboardController.keyboardHandlerReleased(event, player1);
     }
 }
